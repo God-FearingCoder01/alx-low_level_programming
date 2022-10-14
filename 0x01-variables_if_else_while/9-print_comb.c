ea9cbe62eeafe86c46f8e55de5c46219799fcb92
God-FearingCoder01 <mhlopenkosikhona@gmail.com>
2022-10-14 16:07:28 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+		putchar(x, ", ");
+	putchar('\n');
+	return (0);
+}
