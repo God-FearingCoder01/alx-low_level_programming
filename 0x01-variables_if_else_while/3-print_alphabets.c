7af6eb2ba9517c2db6ed59cb5d005129f590e05e
God-FearingCoder01 <mhlopenkosikhona@gmail.com>
2022-10-14 15:27:44 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the whole alphabeti
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 97; x < 122; x++)
+		putchar(x);
+
+	for (x = 65; x <= 90; x++)
+		putcha(x);
+	putchar('\n');
+	return (0);
+}
