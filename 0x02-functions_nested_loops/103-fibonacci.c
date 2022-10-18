f4dfa0734ba1496a3f6035648ebb29f38c092d31
God-FearingCoder01 <mhlopenkosikhona@gmail.com>
2022-10-18 03:24:18 -0700

    Task 15.

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: finds and prints the sum of
+ * the even-valued terms, folowed by a
+ * new line.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	long i, j, n, s;
+	long const x = 4000000;
+
+	i = 1;
+	j = 2;
+
+	s = 2;
+
+	while (j < x)
+	{
+		n = i + j;
+		if (n % 2 == 0)
+			s += n;
+		i = j;
+		j = n;
+	}
+
+	printf("%ld\n", s);
+
+	return {0};
+}
