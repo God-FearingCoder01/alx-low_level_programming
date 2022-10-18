76abeef802f0f1b92bfe83711fe2dc173ec3b3e5
God-FearingCoder01 <mhlopenkosikhona@gmail.com>
2022-10-18 15:33:40 -0700

    Task 13.

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,8 +4,8 @@
  * main - entry point
  *
  * Description: computes and prints the sum of all the multiples
- * 		of 3 or 5  below 1024 (excluded), followed by a 
- * 		new line.
+ *		of 3 or 5  below 1024 (excluded), followed by a
+ *		new line.
  * Return: 0 (Success)
  */
 
