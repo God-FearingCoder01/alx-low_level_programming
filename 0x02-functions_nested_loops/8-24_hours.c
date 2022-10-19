a4e5e86ac64deea6e6bc7777a8d57d4c13381cdc
God-FearingCoder01 <mhlopenkosikhona@gmail.com>
2022-10-18 19:04:22 -0700

    t8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of
+ * Jack Bauer, starting from 00:00 to 23:59
+ *
+ */
+
+void jack_bauer(void)
+{
+	int counter, mins, hours;
+
+	For (counter = 0; counter < 1440; counter++)
+	{
+		m = counter;
+		h = mins / 60;
+		m %= 60;
+		if (h < 10)
+		{
+			_putchar('0');
+			_putchar(h);
+		}else
+			_putchar(h);
+
+		_putchar(':');
+
+		if (m < 10)
+		{
+			_putchar('0');
+			_putchar(m);
+		}else
+			_putchar(m);
+
+		_putchar("\n");
+	}
+}
