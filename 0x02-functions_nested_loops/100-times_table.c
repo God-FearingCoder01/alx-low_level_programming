9078c536d8746f5a30c4d37ac1291dbcd6b312c0
God-FearingCoder01 <mhlopenkosikhona@gmail.com>
2022-10-18 20:43:19 -0700

    agh

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,23 +10,43 @@
 
 void print_times_table(int n)
 {
-	int rowCount, columnCount;
+	int x = 0, y, z;
 
-	if (!((n > 15) || (n < 0)))
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
 	{
-		for (rowCount = 0; rowCount <= n; rowCount++)
+		for (y = 0; y <= n; y++)
 		{
-			for (columnCount = 0; columnCount <= n; columnCount++)
+			z = x * y;
+			if (z > 99)
 			{
-				_putchar(rowCount * columnCount);
-				if (columnCount != n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
 			}
-			_putchar('\n');
-		}
-	}
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
 
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+		x++;
+	}
 }
