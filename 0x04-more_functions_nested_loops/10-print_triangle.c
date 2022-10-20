b88e721fe19ba0872fad6942095ab472d68da5a0
God-FearingCoder01 <mhlopenkosikhona@gmail.com>
2022-10-20 16:31:19 -0700

    Completed task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,17 +11,17 @@
 
 void print_triangle(int size)
 {
+	int i, j, k;
+	
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		int i, j, k;
-
 		for (i = 1; i <= size; i++)
 		{
-			for (j = 1; j < (size - i); j++)
+			for (j = 1; j <= (size - i); j++)
 			{
 				_putchar(' ');
 			}
