f24305e0d0a921cb7a6cfdae2372dc9fe5a4404a
God-FearingCoder01 <mhlopenkosikhona@gmail.com>
2022-10-25 16:32:56 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: the actual string to be reversed
+ *
+ * Return: Nothing
+ */
+
+void rev_string(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+		count++;
+
+	char m[count - 1];
+
+	int i;
+
+	for (i = 0; i < count; i++)
+	{
+		m[i] = s[count - 1];
+		count--;
+	}
+
+	for (i = 0; i < count; i++)
+		s[i] = m[i];
+
+	s[count] = '\0';
+
+}
