e03f339de23b0a29d6de161d02d9169261202b2b
God-FearingCoder01 <mhlopenkosikhona@gmail.com>
2022-10-28 20:19:16 -0700

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,27 +10,22 @@ char *leet(char *arry)
 {
 	int index = 0;
 	char myArry[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char myNums[] = {'4', '3', '0', '1', '7'}
 	int myIndex;
 
 	while (arry[index] != '\0')
 	{
 		myIndex = 0;
 
-		while (arry[index] != myArray[myIndex] && myIndex < 10)
+		while (myIndex < 10)
+		{
+			if (arry[index] == myArray[myIndex] || arry[index] == myArray[5 + myIndex])
+			{
+				arry[index] = myNums[myIndex];
+				myIndex = 10;
+			}
 			myIndex++;
-
-		if (myIndex > 9)
-			;
-		else if (myIndex > 7)
-			arry[index] = 49;
-		else if (myIndex > 5)
-			arry[index] = 55;
-		else if (myIndex > 3)
-			arry[index] = 48;
-		else if (myIndex > 1)
-			arry[index] = 51;
-		else
-			arry[index] = 52;
+		}
 
 		index++;
 	}
