62192d282423b995848a88ae37e57d23dc390563
GFC-01 <mhlopenkosikhona@gmail.com>
2022-11-01 17:09:32 -0700

    Task 7 done

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,5 +4,7 @@
 int _putchar(char c);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
+char *_strstr(char *haystack, char *needle);
 
 #endif
