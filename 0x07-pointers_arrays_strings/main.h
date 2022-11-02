3df76b0739cf3658e384c407cc17be6eef239a78
GFC-01 <mhlopenkosikhona@gmail.com>
2022-11-01 17:41:47 -0700

    Task 1 advanced completed

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
+void set_string(char **s, char *to);
 
 #endif
