1f6466a9e3f76974035c73d74d384e34ee16322e
GFC-01 <mhlopenkosikhona@gmail.com>
2022-11-04 16:25:06 -0700

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Descrription: adds positive numbers, prints
+ *		the result followed by a new line.
+ *		If no number is passed to the program,
+ *		prints 0, followed by a new line
+ *		If one of the number contains symbols
+ *		that are not digits, prints "Error",
+ *		followed by a new line, and return 1.
+ *		You can assume that numbers and the addition
+ *		of all the numbers can be stored in an int
+ * @argc: number of arguemenst apssed
+ * @argv: string of arguments passed
+ * number_contains_symbols - checeks if a given string literal
+ * whichi is supposed ot be composed of numbers only, contains
+ * any other symbols other tahn numbers
+ * @arr: the string to be checked for symbols
+ * get_array_size - finc the length of agiven array or string
+ * @arry: name of array to be used
+ * Return: 0 (onsuccess0
+ */
+
+int get_array_size(char *arry);
+int number_contains_symbols(char *arr);
+
+int main(int argc, char **argv)
+{
+	int sum_result = 0;
+	int k;
+
+	if (argc == 1)
+		printf("%i", 0);
+	else
+	{
+		for (k = 1; k < argc; k++)
+		{
+			if (number_contains_symbols(argv[k]))
+			{
+				printf("Error\n");
+				return (0);
+			}
+			else
+			{
+				sum_result += argv[k];
+			}
+		}
+		printf("%i", sum_result);
+	}
+	return (0);
+}
+
+/**
+ * number_contains_symbols - checeks if a given string literal
+ * whichi is supposed ot be composed of numbers only, contains
+ * any other symbols other tahn numbers
+ * @arr: the string to be checked for symbols
+ * Return: 0 (on success)
+ */
+
+int number_contains_symbols(char *arr)
+{
+	int len_argvElement, char_number, loop_counter;
+
+	len_argvElement = get_array_size(arr);
+
+	char_number = loop_counter = 0;
+
+	for (i = '0'; i <= '9' || char_number < len_argvElement; i++)
+	{
+		if (*arr == i)
+		{
+			char_number++;
+			loop_counter = -1;
+			i = '0';
+		}
+		else if (loop_counter == 9)
+			return (1);
+		loop_counter++;
+	}
+
+	return (0);
+}
+
+/**
+ * get_array_size - finds the length of agiven array or string
+ * @arry: name of array to be used
+ * Return: 0 (on success)
+ */
+
+int get_array_size(char *arry)
+{
+	int array_index = 0;
+
+	while (arry[array_index] != '\0')
+		array_index++;
+
+	return (array_index);
+}
