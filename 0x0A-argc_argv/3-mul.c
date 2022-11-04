e695fae75f502da09aadbb8278a7fef40c8faff8
GFC-01 <mhlopenkosikhona@gmail.com>
2022-11-04 15:26:34 -0700

    Task 3 first attempt

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry poiint
+ * Description: multiples two numbers, and then
+ *		prints the result of the multiplication,
+ *		followed by a new line.
+ *		You can asssume the two numbers and result
+ *		can be stored in an integer.
+ *		If the program doesn't receive two
+ *		arguments, it should print "Error",
+ *		followed by a new line; and then return
+ *		1
+ * @argc: number of command line arguments passed
+ * @argv: string of command line arguments apssed
+ * Return: 0 (on success)
+ */
+
+int main(int argc, char **argv)
+{
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		result = atoi(argv[argc - 1]) * atoi(argv[argc - 2]);
+		printf("%i", result);
+	}
+
+	return (0);
+}
