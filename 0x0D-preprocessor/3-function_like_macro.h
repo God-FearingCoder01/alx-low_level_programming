65933e5ea1f32e84eb46590472cd8464f72142ec
GFC-01 <mhlopenkosikhona@gmail.com>
2022-11-16 17:23:53 -0800

    Task corrected

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
+#ifndef THREE_FUNCTION_LIKE_MACRO_H
+#define THREE_FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) (x * ((x < 0) * (-1) + (x > 0)))
+#define ABS(x) (x > 0 ? (x) : (x) * -1)
 
-#endif /* FUNCTION_LIKE_MACRO */
+#endif /* THREE_FUNCTION_LIKE_MACRO_H */
