c0ee2302f68facc7ecdbb423bb55423417a08d25
GFC-01 <mhlopenkosikhona@gmail.com>
2022-11-16 16:32:08 -0800

    Added comment fro struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,18 @@
-typedef struct dog dog;
+#ifndef DOG_H
+#define DOG_H
 
-struct dog {
-	char* name;
+/**
+ * struct dog - type for my dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of dog
+ */
+
+typedef struct dog
+{
+	char *name;
 	float age;
-	char* owner;
-};
+	char *owner;
+} dog;
+
+#endif
