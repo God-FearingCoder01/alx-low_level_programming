35304f626590736fbf9eddcdf22a4adbf21bb1e2
GFC-01 <mhlopenkosikhona@gmail.com>
2022-11-16 17:40:54 -0800

    Task 3

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,26 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- * _calloc - allocates memory for an array given number of elements and size
- * @nmemb: number of elements
- * @size: size of each element
- * Return: Null if error, else pointer to allocated memory
+ * _calloc - print 0s into 2D grid
+ * @nmemb: array num
+ * @size: size of ea array element
+ * Return: 0
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *p;
+	char *arr;
 	unsigned int i;
 
-	if (nmemb <= 0 || size <= 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(nmemb * size);
-	if (p == NULL)
+	arr = malloc(nmemb * size);
+
+	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
-		p[i] = 0;
+	for (i = 0; i < (nmemb * size); i++)
+		arr[i] = 0;
 
-	return (p);
+	return (arr);
 }
