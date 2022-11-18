67b8dcdc5ca4c10886982c45b503256fe25e2cee
GFC-01 <mhlopenkosikhona@gmail.com>
2022-11-17 16:01:10 -0800

    Correceted spellingfor 'nil'

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		current_value = va_arg(ap, char*);
 
 		if (current_value == NULL)
-			printf("(nill)");
+			printf("(nil)");
 		else
 			printf("%s", current_value);
 
