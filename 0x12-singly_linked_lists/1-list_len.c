a15639cfe8c925a624ef9d1bd83c6d60212b8f1b
GFC-01 <mhlopenkosikhona@gmail.com>
2022-12-15 03:00:27 -0800

    Task 1 completed

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,24 +1,25 @@
-#ilude "lists.h"
+#include "lists.h"
+#include <stdio.h>
 
 /**
- * list_len - Returns the number of elements in a linked `list_t` list
+ * list_len - finds the number of elements of
+ *		`list_t` list
  * @h: head of linked list
- * Return: number of elements in list
+ *
+ * Return: number of elements  of 'list_t' list
  */
 
 size_t list_len(const list_t *h)
 {
-	const list_t *current;
-	size_t c;
+	size_t i = 0;
 
-	c = 0;
-	current = h;
+	if (!h)
+		return (i);
 
-	while (current != NULL)
+	for (i = 1; h->next; i++)
 	{
-		current = current->next;
-		c++;
+		h = h->next;
 	}
 
-	return (c);
+	return (i);
 }
