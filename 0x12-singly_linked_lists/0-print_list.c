6aaf7008f27399e75ae82dfb51a2d0619b73983d
GFC-01 <mhlopenkosikhona@gmail.com>
2022-12-15 01:44:43 -0800

    updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -34,6 +34,6 @@ size_t print_list(const list_t *h)
 
 void print_node(size_t *i, const list_t *h)
 {
-	printf("[%li] %s\n", (h->str) ? *i : 0,
+	printf("[%lu] %s\n", (h->str) ? *i : 0,
 		(h->str) ? (h->str) : ("(nil)"));
 }
