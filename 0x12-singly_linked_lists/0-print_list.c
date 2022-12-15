ea504755d3ba750316ca454a1bb4db669410366e
GFC-01 <mhlopenkosikhona@gmail.com>
2022-12-15 00:47:19 -0800

    created UDF

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,12 +4,25 @@
 /**
  * print_list - print all elements of `list_t` list
  * @h: head of linked list
+ * Description: if 'str' is 'null', print 
+ *		'[0] (nil)'. YOU CAN USE 'printf'
+ *
  * Return: number of nodes
  */
 
 size_t print_list(const list_t *h)
 {
-	const list_t *current;
+	size_t i;
+
+	for (i = 1; h->next; i++)
+	{
+		print_node(&i, h);
+		h = h->next;
+	}
+
+	print_node(&i, h);
+ 
+	/*const list_t *current;
 	size_t c;
 
 	c = 0;
@@ -19,8 +32,20 @@ size_t print_list(const list_t *h)
 		printf("[%d] %s\n", current->len, current->str);
 		current = current->next;
 		c++;
-	}
+	}*/
 
+	return (i);
+}
 
-	return (c);
+/**
+ * print_node - prints a single node of 'list_t'
+ *		list.
+ * @i: the number of node of in list
+ * @h: the node itself
+ */
+
+void print_node(size_t *i, const list_t *h)
+{
+	printf("[%i] %s", (h->str) ? *i : 0,
+		(h->str) ? (h->str) : ("(nil)"));
 }
