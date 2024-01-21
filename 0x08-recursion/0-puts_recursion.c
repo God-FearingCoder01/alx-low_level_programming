6be7811c4775bea3ee79b4a08ca01f6403411cc6
GFC01 <mhlopenkosikhona@gmail.com>
2024-01-21 02:35:15 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,7 +3,12 @@
 
 void _puts_recursion(char *s)
 {
-	if (strlen(s) == 1)
-		return _putchar(s);
-	return _puts_recursion(s);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
